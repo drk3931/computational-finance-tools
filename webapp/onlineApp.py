f9b09081b52e2aa8dd28fb1c20c75986e7c3e449
unknown <drk3931@gmail.com>
2020-09-23 01:43:24 -0400

    reset

--- webapp/onlineApp.py
+++ webapp/onlineApp.py
@@ -25,3 +25,28 @@ def after_request(response):
 
 @app.errorhandler(InvalidUsage)
 def handleError(invUsage):
+    response = jsonify({'error':invUsage.message})
+    response.status_code = invUsage.status_code
+    return response
+
+@app.route('/stockData', methods=["POST"])
+def stockData():
+  reqBody = request.json
+
+  if reqBody is None:
+    raise InvalidUsage('Please include 2 symbols.')
+
+  if 'symbol1' in reqBody and 'symbol2' in reqBody:
+    validSymbol1 = Stocks.validSymbol(reqBody['symbol1'])
+    validSymbol2 = Stocks.validSymbol(reqBody['symbol2'])
+    if validSymbol1 and validSymbol2:
+      chart = Stocks.getPrice(reqBody['symbol1'],reqBody['symbol2'])
+      return chart,200
+    else:
+      raise InvalidUsage('Please enter a valid symbol.',status_code=400)
+  else:
+    raise InvalidUsage('Please include 2 symbols.')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
