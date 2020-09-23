2ee13915da12ce2efab45eb02cb041dc6ecdd055
unknown <drk3931@gmail.com>
2020-09-23 13:30:54 -0400

    removed hardcoded dates and made them relative

--- Stocks.py
+++ Stocks.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import mpld3
 
+
 import datetime as dt
+
 
 import pandas_datareader.data as web
 
@@ -9,12 +11,12 @@ import pandas_datareader.data as web
 
 def validSymbol(symbol):
     try:
-        df = web.DataReader(symbol, 'yahoo', dt.datetime(2020,9,1), dt.datetime(2020,9,2))
+        df = web.DataReader(symbol,'yahoo',dt.date.today() - dt.timedelta(1), dt.date.today())
     except:
         return False
     return True
 
-def getPrice(symbol1,symbol2,startDate = dt.datetime(2020,1,1),endDate = dt.datetime.now()):
+def getPrice(symbol1,symbol2,startDate = dt.date.today() - dt.timedelta(days=180),endDate = dt.date.today()):
     df = web.DataReader(symbol1, 'yahoo', startDate,endDate)
     df2 = web.DataReader(symbol2, 'yahoo', startDate,endDate)
 
@@ -22,6 +24,8 @@ def getPrice(symbol1,symbol2,startDate = dt.datetime(2020,1,1),endDate = dt.date
     prices2 = df2['Adj Close'].pct_change()
 
     fig,ax = plt.subplots()
+    fig.set_figheight(6)
+    fig.set_figwidth(10)
     ax.plot(prices,label=symbol1)
     ax.plot(prices2,label=symbol2)
     ax.legend()
